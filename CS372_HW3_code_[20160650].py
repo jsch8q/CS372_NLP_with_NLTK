4e2e6738629abd0be0acf75f57d10efa562777ed
JSCh <jsch@kaist.ac.kr>
2020-05-17 14:44:26 +0900

    Added cmudict features

--- CS372_HW3_code_[20160650].py
+++ CS372_HW3_code_[20160650].py
@@ -1,7 +1,16 @@
-import stanza, nltk
+import nltk, stanza
 from bs4 import BeautifulSoup
 from urllib import request
 from nltk.corpus import wordnet as wn
+from nltk import pos_tag, word_tokenize
+cmucorpus = nltk.corpus.cmudict 
+
+cmudict_dict = cmucorpus.dict()
+cmuentries = cmucorpus.entries()
+
+def tag_sent(s):
+    txt = word_tokenize(s)
+    return nltk.pos_tag(txt)
 
 def google_query(s):
     return '+'.join(s.split())
@@ -19,17 +28,48 @@ def get_google_search_urls(s):
             new_url = item.split('&amp;sa=')[0][len(href_ind) :]
             res_urls.append(new_url)
     return res_urls
+
+def destress(pronunciation):
+    res = ""
+    for syllable in pronunciation:
+        if syllable[-1].isdigit():
+            res += syllable[:-1]
+        else :
+            res += syllable
+    return res
+
+def heteronym_check_from_cmu(word):
+    pronunciations = cmudict_dict[word]
+    number_of_ways = len(pronunciations)
+    if number_of_ways < 2 :
+        return False
+    destressed_pronunciations = set()
+    for pronunciation in pronunciations:
+        destressed_pronunciation = destress(pronunciation)
+        destressed_pronunciations.add(destressed_pronunciation)
+    if len(destressed_pronunciations) < 2 :
+        return False
+    return True
+
+def heteronyms_from_cmudict():
+    words = [entry[0] for entry in cmuentries]
+    heteronym_candidates = [word for word in words if heteronym_check_from_cmu(word)]
+    """for word in words:
+        if heteronym_check_from_cmu(word):
+            heteronym_candidates.append(word)"""
+    return list(set(heteronym_candidates))
 """
 def bass_def(token = "bass"):
     for item in wn.synsets(token):
         if item.pos() == 'n':
             print(item.definition())
 
+test_sent = "The bandage was wound around the wound."
 
 stanza.download('en')
 nlp = stanza.Pipeline('en')
 
-doc = nlp("Carrey played the bass for the bass which is swimming with the other bass.")
+doc = nlp(test_sent)
 print(*[f'id: {word.id}\tword: {word.text}\thead id: {word.head}\thead: {sent.words[word.head-1].text if word.head > 0 else "root"}\tdeprel: {word.deprel}' for sent in doc.sentences for word in sent.words], sep='\n')
 
 
@@ -41,13 +81,19 @@ instrument = wn.synset('musical_instrument.n.01')
 play = wn.synset('play.v.03')
 drum = wn.synset('drum.n.01')
 """
-
+"""
 google_search = "https://www.google.com/search?q="
 hdr = {'User-Agent': 'Mozilla/5.0'}
 
 urls1 = get_google_search_urls("heteronym pun reddit")
-urls2 = get_google_search_urls("heteronym sentence")
+urls2 = get_google_search_urls("heteronym used in a sentence")
 
 for url in urls1+urls2:
     print(url)
 
+
+print(tag_sent(test_sent))
+"""
+
+hetero_candidates = heteronyms_from_cmudict()
+print(len(hetero_candidates))
